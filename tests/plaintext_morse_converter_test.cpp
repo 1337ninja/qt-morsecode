e4d93a83bcbb264cfcabc1ab7c21392d7639644e
1337ninja <1337ninja@users.noreply.github.com>
2022-07-18 10:22:38 +0530

    Add few tests. Add ctest step for running tests on CI

--- tests/plaintext_morse_converter_test.cpp
+++ tests/plaintext_morse_converter_test.cpp
@@ -10,10 +10,7 @@ class PlaintextMorseConverterTest: public QObject
     Q_OBJECT
 
 private slots:
-    void initTestCase()
-    {}
-
-    void convert_plaintext_to_morse()
+    void convert_valid_plaintext_to_morse()
     {
         PlaintextMorseConverter plaintextMorseConverter;
         std::string actual = plaintextMorseConverter.convert("hello");
@@ -21,8 +18,29 @@ private slots:
         QCOMPARE(expected, actual);
     }
 
-    void cleanupTestCase()
-    {}
+    void convert_invalid_plaintext()
+    {
+        PlaintextMorseConverter plaintextMorseConverter;
+        std::string actual = plaintextMorseConverter.convert("@@");
+        std::string expected("#x#");
+        QCOMPARE(expected, actual);
+    }
+
+    void convert_valid_morse_to_plaintext()
+    {
+        PlaintextMorseConverter plaintextMorseConverter;
+        std::string actual = plaintextMorseConverter.inverseConvert("....x.x.-..x.-..x---");
+        std::string expected("HELLO");
+        QCOMPARE(expected, actual);
+    }
+
+    void convert_invalid_morse_to_plaintext()
+    {
+        PlaintextMorseConverter plaintextMorseConverter;
+        std::string actual = plaintextMorseConverter.inverseConvert("@x@");
+        std::string expected("##");
+        QCOMPARE(expected, actual);
+    }
 };
 
 QTEST_MAIN(PlaintextMorseConverterTest)
