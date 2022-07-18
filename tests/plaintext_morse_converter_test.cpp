8af54ced7208df4ab4a4665ec9c15f546f9cafe9
1337ninja <1337ninja@users.noreply.github.com>
2022-07-18 16:48:54 +0530

    Add few more tests

--- tests/plaintext_morse_converter_test.cpp
+++ tests/plaintext_morse_converter_test.cpp
@@ -18,6 +18,30 @@ private slots:
         QCOMPARE(expected, actual);
     }
 
+    void convert_valid_plaintext_with_spaces_and_nos_to_morse()
+    {
+        PlaintextMorseConverter plaintextMorseConverter;
+        std::string actual = plaintextMorseConverter.convert("IT IS 10 AM");
+        std::string expected("..x-xxx..x...xxx.----x-----xxx.-x--");
+        QCOMPARE(expected, actual);
+    }
+
+    void convert_valid_plaintext_to_morse_last_char_should_not_have_char_delimiter()
+    {
+        PlaintextMorseConverter plaintextMorseConverter;
+        std::string actual = plaintextMorseConverter.convert("AB");
+        std::string expected(".-x-..."); // no 'x' at the last
+        QCOMPARE(expected, actual);
+    }
+
+    void convert_valid_plaintext_to_morse_consecutive_spaces_should_not_have_char_delimiter_in_between_them()
+    {
+        PlaintextMorseConverter plaintextMorseConverter;
+        std::string actual = plaintextMorseConverter.convert("  "); // 2 spaces
+        std::string expected("xxxx"); // should not be "xxxxx"
+        QCOMPARE(expected, actual);
+    }
+
     void convert_invalid_plaintext()
     {
         PlaintextMorseConverter plaintextMorseConverter;
@@ -31,6 +55,14 @@ private slots:
         PlaintextMorseConverter plaintextMorseConverter;
         std::string actual = plaintextMorseConverter.inverseConvert("....x.x.-..x.-..x---");
         std::string expected("HELLO");
+        QCOMPARE(expected, actual);
+    }
+
+    void convert_valid_morse_with_spaces_and_nos_to_plaintext()
+    {
+        PlaintextMorseConverter plaintextMorseConverter;
+        std::string actual = plaintextMorseConverter.inverseConvert("..x-xxx..x...xxx.----x-----xxx.-x--");
+        std::string expected("IT IS 10 AM");
         QCOMPARE(expected, actual);
     }
 
